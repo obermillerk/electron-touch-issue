b183a47c8519b119a53e8d06a06257b825d61092
Karl Obermiller <obewan71@gmail.com>
2017-12-06 17:02:08 -0500

    Added button that logs events

--- renderer.js
+++ renderer.js
@@ -1,3 +1,7 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+$('button').on('click mousedown mouseup touchstart touchend touchcancel touchmove', function(event) {
+    console.log(event.type);
+});
